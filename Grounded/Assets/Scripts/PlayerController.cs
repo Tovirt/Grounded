dbfccf54eb3c856e60bedcdbd6f27acfb3d3c4f8
ToniOjanpera <toni.ojanpera@edu.turkuamk.fi>
2018-01-29 16:21:47 +0200

    Interaction
    lisäsin player scriptiin interactio funktion mikä kutsuu interactattavan objektin Interaction funktiota (spacebar toimii interaction nappina)

--- Grounded/Assets/Scripts/PlayerController.cs
+++ Grounded/Assets/Scripts/PlayerController.cs
@@ -4,33 +4,54 @@ using UnityEngine;
 
 public class PlayerController : MonoBehaviour {
 
-	private Vector3 oldPos;
-	private Vector3 newPos;
     public float speed = 4.0f;
+
+    private Vector2 oldPos;
+	private Vector2 newPos;
+    private Vector2 pointTowards;
     private BoxCollider2D bc2D;
+    private RaycastHit2D intObj;
 
 	void Start () {
         bc2D = GetComponent<BoxCollider2D>();
 	}
 
     void Update () {
+        //Movement of Player
         oldPos = transform.position;
         if (Input.GetKey(KeyCode.A)) {
-            newPos = oldPos + Vector3.left/5;
+            newPos = oldPos + Vector2.left/5;
 		}
 		if(Input.GetKey(KeyCode.D)) {
-            newPos = oldPos + Vector3.right/5;
+            newPos = oldPos + Vector2.right/5;
 		}
 		if(Input.GetKey(KeyCode.W)) {
-            newPos = oldPos + Vector3.up/5;
+            newPos = oldPos + Vector2.up/5;
 		}
 		if(Input.GetKey(KeyCode.S)) {
-			newPos = oldPos + Vector3.down/4;
+			newPos = oldPos + Vector2.down/4;
 		}
+        pointTowards = newPos - oldPos;
         bc2D.enabled = false;
         if (!Physics2D.Linecast(oldPos, newPos))
         {
             transform.position = Vector3.MoveTowards(transform.position, newPos, Time.deltaTime * speed);
+        }
+        bc2D.enabled = true;
+        // Calling Interaction
+        if (Input.GetKeyDown(KeyCode.Space)){
+            Interact(pointTowards);
+        }
+    }
+
+    //Interacting with an object
+    void Interact(Vector2 pointing){
+        Debug.Log("Interaction happened");
+        Vector2 position = transform.position;
+        bc2D.enabled = false;
+        if(intObj = Physics2D.Linecast(position, position + pointing)) {
+            Debug.Log(intObj.collider.name);
+            intObj.collider.SendMessage("Interaction");
         }
         bc2D.enabled = true;
     }
---
+++ Grounded/Assets/Scripts/intCabinCode.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class intCabinCode : MonoBehaviour
+{
+
+    // Use this for initialization
+    void Start()
+    {
+
+    }
+
+    void Interaction()
+    {
+        Debug.Log("You Destroyd the cabinet with a hammer");
+        Destroy(gameObject);
+    }
+}
