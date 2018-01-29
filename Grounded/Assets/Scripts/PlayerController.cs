3828d27d46b485e1f1be07dc3588b185c169e083
ToniOjanpera <toni.ojanpera@edu.turkuamk.fi>
2018-01-29 14:17:21 +0200

    pelaajan liikkuminen
    pelaajan liikkuminen korjattu ei enää yritä liikkua ruutuun jos siinä on jo jotain

--- Grounded/Assets/Scripts/PlayerController.cs
+++ Grounded/Assets/Scripts/PlayerController.cs
@@ -4,38 +4,34 @@ using UnityEngine;
 
 public class PlayerController : MonoBehaviour {
 
-    private Rigidbody2D rb;
 	private Vector3 oldPos;
 	private Vector3 newPos;
     public float speed = 4.0f;
+    private BoxCollider2D bc2D;
 
 	void Start () {
-        rb = GetComponent<Rigidbody2D>();      
+        bc2D = GetComponent<BoxCollider2D>();
 	}
 
     void Update () {
         oldPos = transform.position;
         if (Input.GetKey(KeyCode.A)) {
-            newPos = oldPos + Vector3.left;
+            newPos = oldPos + Vector3.left/5;
 		}
 		if(Input.GetKey(KeyCode.D)) {
-            newPos = oldPos + Vector3.right;
+            newPos = oldPos + Vector3.right/5;
 		}
 		if(Input.GetKey(KeyCode.W)) {
-            newPos = oldPos + Vector3.up;
+            newPos = oldPos + Vector3.up/5;
 		}
 		if(Input.GetKey(KeyCode.S)) {
-			newPos = oldPos + Vector3.down;
+			newPos = oldPos + Vector3.down/4;
 		}
-        transform.position = Vector3.MoveTowards(transform.position, newPos, Time.deltaTime * speed);
-	}
-
-    /*private void FixedUpdate()
-    {
-        float moveHorizontal = Input.GetAxis("Horizontal");
-        float moveVertical = Input.GetAxis("Vertical");
-
-        Vector3 movement = new Vector3(moveHorizontal, moveVertical, 0.0f);
-        rb.velocity = movement * speed;
-    }*/
+        bc2D.enabled = false;
+        if (!Physics2D.Linecast(oldPos, newPos))
+        {
+            transform.position = Vector3.MoveTowards(transform.position, newPos, Time.deltaTime * speed);
+        }
+        bc2D.enabled = true;
+    }
 }
