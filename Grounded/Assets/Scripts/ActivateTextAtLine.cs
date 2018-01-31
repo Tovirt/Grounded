5e4cb344a5423c5caab710f8e81e615aee1deff3
mturune15 <mikko.turunen@edu.turkuamk.fi>
2018-01-31 13:24:50 +0200

    textbox

---
+++ Grounded/Assets/Scripts/ActivateTextAtLine.cs
@@ -0,0 +1,60 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ActivateTextAtLine : MonoBehaviour {
+
+	public TextAsset theText;
+	public int startLine;
+	public int endLine;
+	public TextBoxManager theTextBox;
+	public bool destroyWhenActivated;
+	public bool requireButtonPress;
+	private bool waitForPress;
+
+	void Start () {
+		theTextBox = FindObjectOfType<TextBoxManager> ();
+	}
+	
+
+	void Update () {
+
+		if(waitForPress && Input.GetKeyDown(KeyCode.Space))
+			{
+			theTextBox.ReloadScript (theText);
+			theTextBox.currentLine = startLine;
+			theTextBox.endAtLine = endLine;
+			theTextBox.EnableTextBox ();
+
+			if (destroyWhenActivated) {
+				Destroy (gameObject);
+			}
+		}
+	}
+
+	void OnTriggerEnter2D(Collider2D other)
+	{
+		if (other.name == "Player") {
+			if (requireButtonPress) {
+				waitForPress = true;
+				return;
+			}
+
+			theTextBox.ReloadScript (theText);
+			theTextBox.currentLine = startLine;
+			theTextBox.endAtLine = endLine;
+			theTextBox.EnableTextBox ();
+
+			if (destroyWhenActivated) {
+				Destroy (gameObject);
+			}
+		}
+	}
+
+	void OnTriggerExit2D(Collider2D other)
+	{
+		if (other.name == "Player") {
+			waitForPress = false;
+		}
+	}
+}
---
+++ Grounded/Assets/Scripts/TextBoxManager.cs
@@ -0,0 +1,77 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class TextBoxManager : MonoBehaviour {
+
+	public GameObject textBox;
+	public Text theText;
+	public TextAsset textfile;
+	public string[] textLines;
+
+	public int currentLine;
+	public int endAtLine;
+
+	public PlayerController player;
+
+	public bool isActive;
+	public bool stopPlayerMovement;
+	// Use this for initialization
+	void Start () {
+		player = FindObjectOfType<PlayerController> ();
+
+		if (textfile != null) {
+			textLines = (textfile.text.Split ('\n'));
+		}
+
+		if (endAtLine == 0) {
+			endAtLine = textLines.Length - 1;
+		}
+
+		if (isActive) {
+			EnableTextBox ();
+		} else {
+			DisableTextBox();
+		}
+	}
+
+	void Update(){
+
+		if (!isActive) {
+			return;
+		}
+		theText.text = textLines [currentLine];
+
+		if (Input.GetKeyDown (KeyCode.Space)) {
+			currentLine += 1;
+		}
+
+		if (currentLine > endAtLine) {
+			textBox.SetActive (false);
+		}
+	}
+	public void EnableTextBox()
+	{
+		textBox.SetActive (true);
+		isActive = true;
+
+		if (stopPlayerMovement) {
+			player.canMove = false;
+		}
+	}
+	public void DisableTextBox()
+	{
+		textBox.SetActive (false);
+		isActive = false;
+		player.canMove = true;
+	}
+
+	public void ReloadScript(TextAsset theText)
+	{
+		if (theText != null) {
+			textLines = new string[1];
+			textLines = (theText.text.Split ('\n'));
+		}
+	}
+}
